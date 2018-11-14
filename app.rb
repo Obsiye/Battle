c8382789b5a4145760b17e420e3969b548c80966
Al <>
2018-11-14 14:38:43 +0000

    players' HP initialized

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   enable :sessions
+
   get '/' do
     erb(:index)
   end
@@ -9,12 +10,16 @@ class Battle < Sinatra::Base
   post '/names' do
     session[:player_1_name] = params[:player_1_name]
     session[:player_2_name] = params[:player_2_name]
+    session[:player_1_hp] = 60
+    session[:player_2_hp] = 60
     redirect '/play'
   end
 
   get '/play' do
     @player_1_name = session[:player_1_name]
     @player_2_name = session[:player_2_name]
+    @player_1_hp = session[:player_1_hp]
+    @player_2_hp = session[:player_2_hp]
     erb(:play)
   end
 end
