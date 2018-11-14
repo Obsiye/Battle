9d4394793c843d09fb8c3699fbdc32851946d5fe
Al <>
2018-11-14 15:18:31 +0000

    refactored app to use global variables instead of strings in session

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra/base'
-require 'player'
+require './lib/player.rb'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -9,16 +9,16 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    $player_1_name = Player.new(params[:player_1_name])
+    $player_2_name = Player.new(params[:player_2_name])
     session[:player_1_hp] = 60
     session[:player_2_hp] = 60
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1_name.name
+    @player_2_name = $player_2_name.name
     @player_1_hp = session[:player_1_hp]
     @player_2_hp = session[:player_2_hp]
     erb(:play)
