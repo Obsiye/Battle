2f4f592a217af5dcb7dc1c2ca86d7e9685095bc0
**namehere** <**emailhere**>
2018-11-15 15:12:00 +0000

    Feature test up player 1 HP, confirmation of player two attacks

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -2,6 +2,24 @@ feature "battle" do
   let(:default_hp) { Player::DEFAULT_HP }
   let(:default_damage) { Player::DEFAULT_DAMAGE }
 
+  scenario "attacking Player 1" do
+    sign_in_and_play
+    click_button "Attack"
+    click_button "OK"
+    click_button "Attack"
+    expect(page).to have_content "Al attacks Abdi"
+  end
+
+  scenario "reduce Player 1 HP" do
+    sign_in_and_play
+    2.times do
+      click_button "Attack"
+      click_button "OK"
+    end
+    new_hp = default_hp - default_damage
+    expect(page).to have_content "Abdi HP: #{new_hp}"
+  end
+
   scenario "attacking Player 2" do
     sign_in_and_play
     click_button "Attack"
--- spec/features/hp_spec.rb
+++ spec/features/hp_spec.rb
@@ -1,6 +1,11 @@
 feature "hp" do
   let(:default_hp) { Player::DEFAULT_HP }
 
+  scenario "View player 1 hp" do
+    sign_in_and_play
+    expect(page).to have_content "Abdi HP: #{default_hp}"
+  end
+
   scenario "view Player 2 hp" do
     sign_in_and_play
     expect(page).to have_content "Al HP: #{default_hp}"
